0a88f7114e740ddffd724c8a265fa574f4ee1b7a
Aaron Voelker <arvoelke@gmail.com>
2014-03-13 11:23:53 -0400

    Fixed an auto-labelling case

--- nengo_helper.py
+++ nengo_helper.py
@@ -9,7 +9,9 @@ import util
 
 def annotate_object(cls, *args, **kwargs):
     """Annotate NengoObject obj, given the initialized *args, **kwargs."""
-    need_auto_label = (kwargs.get('label', '') == '')
+    new_label = None
+    need_auto_label = 'label' not in kwargs and isinstance(
+        cls, (nengo.objects.Neurons, nengo.Node, nengo.Ensemble, nengo.Network))
     for fn, line, function, code in reversed(traceback.extract_stack()):
         # Find the code which actually created this object. This is the deepest
         # call that is not in this file or nengo.objects. This is different
@@ -20,7 +22,7 @@ def annotate_object(cls, *args, **kwargs):
                 not util.eq_py_filenames(fn, nengo.objects.__file__) and \
                 not util.eq_py_filenames(fn, __file__):
             if code:
-                kwargs['label'] = util.parse_variable_name(code)
+                new_label = util.parse_variable_name(code)
                 need_auto_label = False
         # Find the deepest line in the user's code that created this object
         if fn == '<string>':
@@ -33,6 +35,7 @@ def annotate_object(cls, *args, **kwargs):
     else:
         cls._deepest_line_number = 0
     cls._need_auto_label = need_auto_label
+    return new_label
 
 
 def initialize():
@@ -41,7 +44,9 @@ def initialize():
     old_init = nengo.NengoObject.__init__
 
     def patched_init(cls, *args, **kwargs):
-        annotate_object(cls, *args, **kwargs)
+        new_label = annotate_object(cls, *args, **kwargs)
+        if new_label:
+            kwargs['label'] = new_label
         old_init(cls, *args, **kwargs)
 
     nengo.NengoObject.__init__ = patched_init
